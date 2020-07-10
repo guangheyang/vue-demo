3b69ee1aa5820a5b321e4d7f60df423f729889bb
yangguanghe <1649063797@qq.com>
2020-07-10 23:29:14 +0800

    函数式组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
+<template>
+  <div id="app">
+    <base-level :level="1" @click="handleClick">
+      <template #default="{ text }">
+        {{text}}
+        标题
+      </template>
+      <div>div</div>
+      <p>p</p>
+    </base-level>
+  </div>
+</template>
 <script>
-import BaseDemo from './components/BaseDemo'
+import BaseLevel from './components/BaseLevel'
 export default {
   name: 'App',
   components: {
-    BaseDemo
+    BaseLevel
+  },
+  provide() {
+    return {
+      name: 'yang'
+    }
   },
   data() {
     return {
@@ -14,54 +31,9 @@ export default {
     }
   },
   methods: {
-    vIf() {
-      if(this.num === 1) {
-        return <div>1</div>
-      } else if(this.num === 2) {
-        return <span>1</span>
-      } else {
-        return <p>{ this.num }</p>
-      }
-    },
-    handleClick(num) {
-      console.log(num)
+    handleClick() {
+      console.log('click')
     }
-  },
-  mounted() {
-    console.log(this.$refs)
-  },
-  render() {
-     const scopedSlots = {
-      scopedSlots: {
-        default: props => <span>{ props.text }</span>,
-      }
-    };
-    return (
-      <div>
-        <div>{ this.name }</div>
-        <div domPropsInnerHTML="<a>a</a>"></div>
-        <div domPropsTextContent="<a>a</a>"></div>
-        <div v-show={ this.show }>show</div>
-        { true && <div>v-if</div> }
-        { this.show ? <div>div</div> : <span>span</span> }
-        { this.vIf() }
-        <ul>
-          { [1, 2, 3].map(item => <li key={ item } ref="li" refInFor={true}>{ item }</li>) }
-        </ul>
-        <div on-click={ this.handleClick }>点我</div>
-        <base-demo nativeOnClick={ this.handleClick }></base-demo>
-        <div on-click={ () => this.handleClick(1) }>点击</div>
-        <div class={['a', 'b']} style={ { fontSize: '14px', color: 'red' } }>v-bind</div>
-        <input type="text" ref="input" v-model={ this.content } />
-        { this.content }
-        <base-demo>
-          <div slot="default">默认插槽</div>
-          <div slot="header">具名header插槽</div>
-        </base-demo>
-        <base-demo {...scopedSlots}>
-        </base-demo>
-      </div>
-    )
   }
 }
 </script>
---
+++ src/components/BaseLevel.vue
@@ -0,0 +1,35 @@
+<template functional>
+  <div>
+    {{ props.level }}
+  </div>
+</template>
+<script>
+export default {
+  functional:true,
+  inject: ['name'],
+  props: {
+    level: {
+      type: Number,
+      required: true
+    }
+  },
+  render (h, context) {
+    console.log(context)
+    const { props, slots, scopedSlots, data, parent, listeners, injections, children } = context
+    const tag = 'h' + props.level
+    console.log(data, 'data')
+    console.log(parent, 'parent')
+    console.log(listeners, data.on, 'listeners')
+    console.log(injections, 'injections')
+    console.log(children, 'children')
+    return (
+    <tag>
+      { slots().default }
+      { scopedSlots.default({ text:"默认插槽" }) }
+      { injections.name }
+      <div>{children}</div>
+    </tag>
+  )
+  }
+}
+</script>
