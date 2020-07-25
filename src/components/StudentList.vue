1cccc2615132de7fdcc3faa3990ae160bd0bb689
yangguanghe <1649063797@qq.com>
2020-07-25 21:36:18 +0800

    state

---
+++ src/components/StudentList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="student-list">
+        <div>学院列表：</div>
+        <ul>
+            <li v-for="item in studentList" :key="item.id">姓名:{{item.name}} 年龄:{{item.age}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+    computed: mapState(['studentList'])
+}
+</script>
+
+<style scoped>
+    div {
+        margin-bottom: 15px;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="home">
     首页
+    <button @click="$store.state.count++">点击</button>
+    {{ storeCount }}
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data() {
+    return {
+      // count: this.$store.state.count
+      count: 100
+    }
+  },
+  computed: {
+    // count() {
+    //   return this.$store.state.count
+    // }
+    // ...mapState(['count'])
+    ...mapState({
+      // storeCount: (state) => {
+      //   return state.count + 10
+      // }
+      storeCount: 'count'
+    })
+  },
+  created() {
+    console.log(this.$store)
+  }
+}
+</script>
