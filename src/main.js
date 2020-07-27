b0f2beae1ea9a680921b91b3624e2b5e26770146
yangguanghe <1649063797@qq.com>
2020-07-28 01:01:07 +0800

    Vuex原理-state-module

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import "./assets/reset.css";
 import router from './router'
+import store from './store'
+
 Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="home">
-    首页
+    {{$store.state.count}}
+    {{ $store.state.student.num }}
+    <button @click="handleClick">点击</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      this.$store.state.count++
+      this.$store.state.student.num++
+    }
+  }
+}
+</script>
